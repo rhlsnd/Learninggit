5c02464d3647e7c741a20403ff893702b7a2e3f8
“rhlsnd” <sanadirahil@gmail.com>
2022-10-13 15:23:29 +0530

    checkpoint

---
+++ Desktop/movies-app-main-main/movies-app-main-main/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from '../src/screens/home/Home';
+import HomeDetails from '../src/screens/details/HeaderDetails';
+import reportWebVitals from './reportWebVitals';
+import Details from'../src/screens/details/Details';
+import App from './common/App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Home/>
+    <App/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
