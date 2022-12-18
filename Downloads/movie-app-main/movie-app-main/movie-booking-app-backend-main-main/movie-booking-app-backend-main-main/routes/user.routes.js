bf80711250adb1de638831e6e53f3ab874b15d60
“rhlsnd” <sanadirahil@gmail.com>
2022-12-18 19:56:18 +0530

    final commit

---
+++ Downloads/movie-app-main/movie-app-main/movie-booking-app-backend-main-main/movie-booking-app-backend-main-main/routes/user.routes.js
@@ -0,0 +1,12 @@
+module.exports = (router) => {
+    const userController = require("../controllers/user.controller");
+
+    router.post("/signup", userController.signUp);
+    router.post("/login", userController.login);
+    router.post("/logout", userController.logout);
+
+
+    router.get("/coupons", userController.getCouponCode);
+    router.put("/bookshow", userController.bookShow);
+    return router;
+  } 
